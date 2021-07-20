b2d03d5fb9aac3d67f387311b82ad379d8f18e2e
wilsonangga <angcungsen@gmail.com>
2021-07-20 15:03:33 +0700

    daily update

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -6,18 +6,11 @@
         @include('_posts-header')
 
         <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
-            <x-post-feature-card/>
-
-            <div class="lg:grid lg:grid-cols-2">
-                <x-post-card/>
-                <x-post-card/>
-            </div>
-
-            <div class="lg:grid lg:grid-cols-3">
-                <x-post-card/>
-                <x-post-card/>
-                <x-post-card/>
-            </div>
+            @if($posts->count())
+                <x-posts-grid :posts="$posts"/>
+            @else
+                <p class="text-center">No posts yet. Please check back later.</p>
+            @endif
         </main>
 
 @endsection
